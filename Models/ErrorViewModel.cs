f95a2e7b7adc1bcbba2c07d028c83006ba326ffa
Noncorporeal <42417289+Noncorporeal@users.noreply.github.com>
2022-11-18 12:18:43 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Consto_Uni.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
