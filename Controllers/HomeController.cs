4d2cd1885c1d6f048acdce4ba083ac491bbc7e3b
Noncorporeal <42417289+Noncorporeal@users.noreply.github.com>
2022-11-19 11:26:32 -0700

    It works now

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,40 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Consto_Uni.Models;
+using Microsoft.EntityFrameworkCore;
+using Consto_Uni.Data;
+using Consto_Uni.Models.SchoolViewModels;
+using Microsoft.Extensions.Logging;
 
 namespace Consto_Uni.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly SchoolContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, SchoolContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+
+    public async Task<ActionResult> About()
+    {
+        IQueryable<EnrollmentDateGroup> data =
+            from student in _context.Students
+            group student by student.EnrollmentDate into dateGroup
+            select new EnrollmentDateGroup()
+            {
+                EnrollmentDate = dateGroup.Key,
+                StudentCount = dateGroup.Count()
+            };
+        return View(await data.AsNoTracking().ToListAsync());
     }
 
     public IActionResult Privacy()
--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Consto_Uni.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SchoolContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SchoolContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -21,8 +44,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
